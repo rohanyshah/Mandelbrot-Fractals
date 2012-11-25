bdd84708416872d425ff7f3d866e8b4456f541cf
Rohan Shah <rohan.shah020@gmail.com>
2012-11-25 13:32:12 -0500

    Mandelbrot's Fractals
    Explore the fascinating fractals in objective-c

---
+++ RoundedTextField.h
@@ -0,0 +1,16 @@
+//
+//  RoundedTextView.h
+//  Mandelbrot
+//
+//  Created by Rohan Shah on 12/24/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface RoundedTextField : NSTextField {
+
+}
+
+@end
